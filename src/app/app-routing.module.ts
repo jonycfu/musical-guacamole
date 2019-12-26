e61c339a701db8db8219536f593c2da6e3b0d404
jonycfu <jon.yc.fu@gmail.com>
2019-12-26 12:03:29 +0100

    Fix list component prefix; Add Score feature for routing purposes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
   },
   {
+    path: "scoreboard",
+    loadChildren: () => import("./score/score.module").then(m => m.ScoreModule)
+  },
+  {
     // matches all non-matching paths to redirect to menu screen
     path: "",
     redirectTo: "start-menu",
