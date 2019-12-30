63558981290143dd7171e31dfb57e01141dddcdb
jonycfu <jon.yc.fu@gmail.com>
2019-12-30 06:55:48 +0100

    Created scoreboard for scorekeeping functionality

---
+++ src/app/game/pages/scoreboard/scoreboard.component.ts
@@ -0,0 +1,28 @@
+import { Observable, Subscription } from 'rxjs';
+import { IScore } from './../../reducers/game.reducer';
+import { loadScores } from './../../actions/game.actions';
+import { Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IGameState, getHighScores } from '../../reducers/game.reducer';
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.scss'],
+})
+export class ScoreboardComponent implements OnInit, OnDestroy {
+  scoreList: Array<IScore>;
+  storeSub: Subscription;
+  constructor(private store: Store<IGameState>) {}
+
+  ngOnInit() {
+    this.store.dispatch(loadScores());
+    this.storeSub = this.store.select(getHighScores).subscribe(scoreList => {
+      this.scoreList = scoreList.slice().sort((a, b) => b.score - a.score);
+    });
+  }
+
+  ngOnDestroy() {
+    this.storeSub.unsubscribe();
+  }
+}
