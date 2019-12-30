2a7d3474ae10830d03c58f936d9d1b22b471f9f7
jonycfu <jon.yc.fu@gmail.com>
2019-12-30 06:50:32 +0100

    Update server.js for score requests

--- express-api-server/server.js
+++ express-api-server/server.js
@@ -15,12 +15,36 @@ const WORD_POOL_LIST = [
 ];
 
 const HIGH_SCORE_LIST = [
-  { name: 'John Bell', score: 390 },
-  { name: 'Jill Bell', score: 490 },
-  { name: 'Jake Bell', score: 130 },
-  { name: 'Jordan Bell', score: 200 },
-  { name: 'Jack Bell', score: 300 },
-  { name: 'Jenny Bell', score: 130 },
+  {
+    name: 'John Bell',
+    score: 390,
+    datetime: new Date('December 17, 1995 03:24:00'),
+  },
+  {
+    name: 'Jill Bell',
+    score: 490,
+    datetime: new Date('October 19, 1995 03:24:00'),
+  },
+  {
+    name: 'Jake Bell',
+    score: 130,
+    datetime: new Date('October 19, 1995 03:24:00'),
+  },
+  {
+    name: 'Jordan Bell',
+    score: 200,
+    datetime: new Date('October 19, 1995 03:24:00'),
+  },
+  {
+    name: 'Jack Bell',
+    score: 300,
+    datetime: new Date('October 19, 1995 03:24:00'),
+  },
+  {
+    name: 'Jenny Bell',
+    score: 130,
+    datetime: new Date('October 19, 1995 03:24:00'),
+  },
 ];
 
 server.get('/secret-words', function(req, res, next) {
@@ -29,6 +53,17 @@ server.get('/secret-words', function(req, res, next) {
 
 server.get('/high-scores', function(req, res) {
   res.json({ payload: HIGH_SCORE_LIST });
+});
+
+server.put('/high-scores', function(req, res) {
+  console.log(req);
+
+  res.json({
+    payload: {
+      code: 200,
+      message: `Update Success:`,
+    },
+  });
 });
 
 //TODO: provide dotenv to read $PORT
