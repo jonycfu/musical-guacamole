7931dd27297e0fd12e12a0a3eff5f3708f72bf20
jonycfu <jon.yc.fu@gmail.com>
2020-01-01 23:22:11 +0100

    Add unit-test to victory|scoreboard pages

--- src/app/game/pages/victory/victory.component.spec.ts
+++ src/app/game/pages/victory/victory.component.spec.ts
@@ -1,3 +1,5 @@
+import { saveScores } from './../../actions/game.actions';
+import { of } from 'rxjs';
 import { getFinalScore } from './../../reducers/game.reducer';
 import { ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
@@ -6,6 +8,7 @@ import { VictoryComponent } from './victory.component';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { initialState, IGameState } from '../../reducers/game.reducer';
 import { MemoizedSelector, Store } from '@ngrx/store';
+import { hot, cold } from 'jasmine-marbles';
 
 describe('VictoryComponent', () => {
   let component: VictoryComponent;
@@ -31,5 +34,28 @@ describe('VictoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the gameScore upon OnInit', done => {
+    const spy = spyOn(store, 'select').and.returnValue(of(getFinalScore));
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith(getFinalScore);
+    done();
+  });
+
+  it('should dispatch saveScores on submit of score form', done => {
+    const datetime = new Date('1995-12-17T03:24:00');
+    const expectedScoreData = { name: 'John Doe', score: 9999, datetime };
+    component.datetime = datetime;
+    component.gameScore = expectedScoreData.score;
+    component.scoreForm.value.name = expectedScoreData.name;
+    const spy = spyOn(store, 'dispatch');
+    component.onSubmit();
+
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(saveScores(expectedScoreData));
+    done();
   });
 });
